242b9ea8e68739193be0e053d9a8e228d557ee9f
Raynor Kirkson E. Chavez <raynor.chavez@192.168.254.171>
2023-08-21 11:10:33 +0800

    Initial scraper folder structure

---
+++ platforms/shopify/scraper.py
@@ -0,0 +1,8 @@
+from core.base_scraper import BaseScraper
+
+class ShopifyScraper(BaseScraper):
+    def get_product_data(self):
+        # scraping logic for Shopify
+        pass
+
+    # ...  Shopify-specific methods
